40ffe23a918ad68e3d7697073e64d039af0ca60f
andremelzc <andre.melendezc@unmsm.edu.pe>
2025-11-04 00:07:21 -0500

    fix: update image remotePatterns to allow any hostname for improved flexibility

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "lh3.googleusercontent.com",
+        hostname: "**", // Permite CUALQUIER hostname
       },
       {
-        protocol: 'https',
-        hostname: 'd1m7bwa5z2c7c9.cloudfront.net',
+        protocol: "http", // Opcional: si también usas http
+        hostname: "**",
       },
     ],
   },
