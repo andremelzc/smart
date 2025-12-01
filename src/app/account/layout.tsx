2539d9e5399c5a96b7c2874e2f9ec3abc01d8721
andremelzc <andre.melendezc@unmsm.edu.pe>
2025-11-30 23:48:25 -0500

    refactor: clean up layout components and improve sidebar styling

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-
 import AccountNavbar from "@/src/components/layout/AccountNavbar";
-
 import AccountSidebar from "@/src/components/layout/AccountSidebar";
 
 export default function AccountLayout({
@@ -12,20 +10,16 @@ export default function AccountLayout({
   return (
     <div className="flex h-screen flex-col bg-gray-50">
       {/* Account Navigation - Fijo arriba */}
-
       <AccountNavbar />
 
       {/* Main Container - Ocupa el resto de la altura */}
-
       <div className="flex flex-1 overflow-hidden">
         {/* Sidebar - Fijo a la izquierda con scroll interno si es necesario */}
-
         <div className="flex-shrink-0">
           <AccountSidebar />
         </div>
 
         {/* Content - Scroll independiente */}
-
         <main className="flex-1 overflow-y-auto bg-white">
           <div className="p-8">
             <div className="mx-auto max-w-7xl">{children}</div>
--- src/app/host/layout.tsx
+++ src/app/host/layout.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-
 import HostNavbar from "@/src/components/layout/HostNavbar";
-
 import HostSidebar from "@/src/components/layout/HostSidebar";
 
 export default function HostLayout({
@@ -12,23 +10,19 @@ export default function HostLayout({
   return (
     <div className="flex h-screen flex-col bg-gray-50">
       {/* Host Navigation - Fijo arriba */}
-
       <HostNavbar />
 
       {/* Main Container - Ocupa el resto de la altura */}
-
       <div className="flex flex-1 overflow-hidden">
         {/* Sidebar - Fijo a la izquierda con scroll interno si es necesario */}
-
         <div className="flex-shrink-0">
           <HostSidebar />
         </div>
 
         {/* Content - Scroll independiente */}
-
         <main className="flex-1 overflow-y-auto bg-white">
-          <div className="px-6 py-8 lg:px-10">
-            <div className="mx-auto w-full max-w-7xl">{children}</div>
+          <div className="p-8">
+            <div className="mx-auto max-w-7xl">{children}</div>
           </div>
         </main>
       </div>
